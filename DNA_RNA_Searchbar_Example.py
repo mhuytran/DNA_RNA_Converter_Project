7d887b3fa57eb85f9e517ec0ca88b87ce8458b0a
Huy Tran <huyjosephtran@gmail.com>
2020-09-17 23:09:14 -0700

    Example #1

--- DNA_RNA_Searchbar_Example.py
+++ DNA_RNA_Searchbar_Example.py
@@ -1,4 +1,4 @@
-import converterFunctions as cf
+import nucleicfunctions as nf
 
 # This is a searchbar program that utilizes the functions from the converterFunctions script. 
 # First, the user inputs the type of nucleic acid desired for template strand conversion. Second, the user
@@ -8,42 +8,35 @@ import converterFunctions as cf
 
 #------------------------------------------------------------------------------------------------------------------
 print("\n"+"Prompt".center(86, "-"))
-print("Please type DNA or RNA into the search bar:\n")
+print("Please enter DNA or mRNA into the search bar:\n")
 
-strand = input().strip()
+strand = input().strip().lower()
 
-print("\nPlease type 5-3 or 3-5 for the directionality of your template strand:\n")
+print("\nPlease enter 5-3 or 3-5 for the directionality of your DNA template strand. If mRNA strand, enter 5-3. \n")
 
 check_strand = input().strip()
 
 print("\n" + "Confirmation: " + strand + ", " + check_strand)
 
 #-----------------------------------------------------------------------------------------------------------------
-if (check_strand == '5-3' and strand == 'DNA'):
+if (check_strand == '5-3' and strand == 'dna'):
     
     print("\nNow, please type in your 'DNA' template strand: \n")
     dna_template_strand = input().upper()
-    print(cf.convert_dna_5_3(dna_template_strand))
+    print(nf.convert_dna_5_3(dna_template_strand))
 
-elif (check_strand == '3-5' and strand == 'DNA'):
+elif (check_strand == '3-5' and strand == 'dna'):
     
     print("\nNow, please type in your 'DNA' template strand: \n")
     dna_template_strand = input().upper()
-    print(cf.convert_dna_3_5(dna_template_strand))
-    
-    
-elif (check_strand == '5-3' and strand == 'RNA'):
-    
-    print("\nNow, please type in your 'RNA' template strand: \n")
-    rna_template_strand = input().upper()
-    print(cf.convert_rna_5_3(rna_template_strand))
+    print(nf.convert_dna_3_5(dna_template_strand))
 
-elif (check_strand == '3-5' and strand == 'RNA'):
+elif (check_strand == "5-3" and strand == 'mrna'):
     
-    print("Now, please type in your 'RNA' template strand: ")
-    rna_template_strand = input().upper()
-    print(cf.convert_rna_3_5(rna_template_strand))
-    
+    print("\nNow, please type in your 'mRNA' template strand: \n")
+    mRNA_template_strand = input().upper()
+    print(nf.convert_rna(mRNA_template_strand))
+  
 else:
     raise Exception("Please re-run code, you did not enter the correct syntax of either the nucleic acid or directionality")
 
