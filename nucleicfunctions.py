79870a9e53f757808f5b5c13083959d74344b0e5
Minh-Huy Tran <71237489+mhuytran@users.noreply.github.com>
2020-09-27 23:50:26 -0700

    functions!

--- nucleicfunctions.py
+++ nucleicfunctions.py
@@ -11,8 +11,9 @@ def convert_dna_decorator_5_3(func):
     """
        This convert_dna_decorator_5_3 decorator prints out the results of the convert_dna_5_3 function: 
        the complementary DNA strand, the base-pairing representation, and the mRNA strand that is synthesized from 
-       the complementary DNA strand. In addition, the mRNA strand is copied to the clipboard if the user wants utilize the 
-       convert_rna function.
+       the complementary DNA strand. In addition, the mRNA strand is copied to the clipboard for the user to have the option
+       to pass the mRNA strand into the convert_rna function by pressing ctrl + v.
+  
         
     Args:
         func([str]): The return value of convert_dna_5_3: a tuple consisting of the complementary_dna_strand,
@@ -133,7 +134,9 @@ def convert_dna_decorator_3_5(func):
     """
        This convert_dna_decorator_5_3 decorator prints out the results: the complementary DNA strand, the
        base-pairing representation, and the mRNA strand that is synthesized from the template strand.
-       In addition, the mRNA strand is copied to the clipboard if the user wants to utilize the convert_rna function.
+       In addition, the mRNA strand is copied to the clipboard for the user to have the option
+       to pass the mRNA strand into the convert_rna function by pressing ctrl + v.
+  
        
     Args:
         func ([str]): The return value of convert_dna_3_5: a tuple consisting of the complementary_dna_strand
