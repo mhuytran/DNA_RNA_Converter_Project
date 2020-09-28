2fcc2c9cf3da471ebd73222b3762113bb0346746
Minh-Huy Tran <71237489+mhuytran@users.noreply.github.com>
2020-09-27 21:55:38 -0700

    Update DNA_RNA_Normal_Example.py

--- DNA_RNA_Normal_Example.py
+++ DNA_RNA_Normal_Example.py
@@ -3,9 +3,9 @@ import nucleicfunctions as nf
 #--------------------------------------------------------------------------------------------------------
 
 # This is a basic use of the functions in the converterFunctions script.
-# The convert_dna_5_3 function will copy the outputted mRNA strand to the clipboard, 
-# and can be inputted into the convert_rna function. This process can also be done with
-# the convert_dna_5_3 function
+# The convert_dna_5_3 function displays its results, and copies the outputted mRNA strand to the clipboard.
+# The mRNA strand can then be pasted into the convert_rna function, which will display its results. 
+# Similarly, this process can be done with the convert_dna_3_5 function
 
 #--------------------------------------------------------------------------------------------------------
 
